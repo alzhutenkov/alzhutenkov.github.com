4897bd57b43318e7f754d3b3fc161ea1212c18a6
Aleksey Zhutenkov <zhutenkov.a.s@sberbank.ru>
2022-06-22 21:35:24 +0300

    added new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "bec542c9839c677cb9b5274b77e8366e",
-"index.html": "56f12d4baa80fb4e2a06b53d39b3203f",
-"/": "56f12d4baa80fb4e2a06b53d39b3203f",
-"main.dart.js": "5143764e734d538f15687cc7e95acd0f",
+"index.html": "5b6e16aaa58056b0d3dfd886c7ce6fc6",
+"/": "5b6e16aaa58056b0d3dfd886c7ce6fc6",
+"main.dart.js": "35fa0c86fa606e702bef0e77a49de88d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
